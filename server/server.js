a89306ce3406e223f886a3c9b75af9ae5bc20ff3
stormfire-ll <114418651+stormfire-ll@users.noreply.github.com>
2023-06-17 21:48:12 +0200

    - restructured files in folder - endpoints for: login, register form

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-//1. Tutorial
+const express = require('express')
+const path = require('path')
 const http = require('http') 
 const fs = require('fs')    //library for file handling
+
+const app = express();
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'files')))
+
+//Server setup
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-type': 'text/html' })
     fs.readFile('index.html', function(error, data){
@@ -16,9 +22,18 @@ const server = http.createServer(function(req, res){
     }) 
 })
 
+//Endpoints: User Authentication
+app.get('/register', (req, res) => { 
+    res.render('register.html')
+})
+
+app.get('/login'), (req, res) => {
+    res.render('login.html')
+}
+
 server.listen(port, function(error) { //function called if error: pass error or nothing if successful
     if (error) {
-        console.log('Something went wrong', error) //!no log
+        console.log('Listening on port' + port + 'failed.', error) //!no log
     } else {
         console.log('Server is listening on port' + port)
     }
