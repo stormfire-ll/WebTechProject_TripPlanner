7eba5ccdd6696e4157087050e25b87371ff34177
stormfire-ll <114418651+stormfire-ll@users.noreply.github.com>
2023-06-27 00:36:41 +0200

    weather.js: addedEventlistener

--- public/javascript/currencyconverter.js
+++ public/javascript/currencyconverter.js
@@ -1,1 +1,24 @@
-module.exports = {};
+function convertCurrency() {
+    //console.log("convertCurrency")
+    let input = document.getElementById("amount").value;
+    let selectedItem = document.getElementById("select").value;
+    //console.log(selectedItem);
+
+    fetch("/currencyconverter")
+    .then(async response => await response.json()) //parse JSONString -> JSObject
+    .then( response => {
+        const primaryCurrency = document.getElementById("select");
+        primaryCurrency.innerHTML = getOptions(currencies);
+
+        function getOptions(data) {
+            return Object.entries(data)
+              .map(([country, currency]) => `<option value="${country}">${country} | ${currency}</option>`)
+              .join("");
+          }
+
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    
+}
