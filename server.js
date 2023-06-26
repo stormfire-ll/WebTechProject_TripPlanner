9accd4974b50aaffd3d6a3415d892040c3647b1a
stormfire-ll <114418651+stormfire-ll@users.noreply.github.com>
2023-06-26 16:12:30 +0200

    @Florentina: currencyconverter.html: editierbar

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios')
 const session = require('express-session');
 const bodyParser = require("body-parser");
-const axios = require('axios')
+const currencyModel = require("./public/javascrip/currency-list.js");
 
 // Serve static views from the "public" directory
-app.use(express.static(path.join(__dirname, '/public')));   // Maksym
-app.use(express.static(path.join(__dirname, '/server')));   // Ting
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, '/public/html' ));
 app.use(
@@ -42,29 +41,28 @@ app.post('/signup', async (req, res) => {
    res.render('login');
 })
 
-//WeatherAPI: 1. weather.ejs
+//weather endpoint fetches Weater API data: weather.js
 app.get("/weather", async (req, res) => {
    
    const weatherAPIKey = "17dc0ef21a4041da85d6fa0e6119fe49";
    const city = req.query.city;     // query parameter to get city
    
-   const cityWeather = axios.get("https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + weatherAPIKey + "&q=" + city)
-   .then(result =>{   
-      let filteredData = {
-         "temperature": result.data.main.temp,
-         "wind": result.data.wind.speed,
-         "city": result.data.name,
-         "day": new Date().toLocaleDateString('en-EN', {"weekday": "long"}),
-         "humidity": result.data.main.humidity,
-      };         
-      res.json(JSON.stringify(filteredData))
-      console.log(result.data);
-   })
-   .catch(error => {
-         console.log(error);
-   });
+   axios.get("https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + weatherAPIKey + "&q=" + city)
+      .then(result =>{   
+         let filteredData = {
+            "temperature": result.data.main.temp,
+            "wind": result.data.wind.speed,
+            "city": result.data.name,
+            "day": new Date().toLocaleDateString('en-EN', {"weekday": "long"}),
+            "humidity": result.data.main.humidity,
+         };         
+         res.json(JSON.stringify(filteredData))
+         console.log(result.data);
+      })
+      .catch(error => {
+            console.log(error);
+      });
 });
-
 
 
 
